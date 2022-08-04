3bf4bb1b3baaf0cbeed64a4952251ea4bab94d0d
Anderson <BlackCode7@outlook.com>
2022-08-03 21:56:05 -0300

    Iniciando projeto para teste de frontend e backend

---
+++ Projeto_1/PacoteBackEndProduto/src/main/java/com/BackEndProduto/PacoteBackEndProduto/PacoteBackEndProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.BackEndProduto.PacoteBackEndProduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacoteBackEndProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacoteBackEndProdutoApplication.class, args);
+	}
+
+}
---
+++ Projeto_1/PacoteBackEndProduto/src/test/java/com/BackEndProduto/PacoteBackEndProduto/PacoteBackEndProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BackEndProduto.PacoteBackEndProduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacoteBackEndProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
