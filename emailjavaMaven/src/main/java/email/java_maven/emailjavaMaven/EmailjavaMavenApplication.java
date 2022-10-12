0aa5086399729d9dadf07002e43131eaa444cd4a
BlackCode7 <seven.metais@outlook.com>
2022-10-12 18:41:16 -0300

    Estudo de envio de e-mails com JavaMail - Falta configurar o google SMTP

---
+++ emailjavaMaven/src/main/java/email/java_maven/emailjavaMaven/EmailjavaMavenApplication.java
@@ -0,0 +1,13 @@
+package email.java_maven.emailjavaMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailjavaMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailjavaMavenApplication.class, args);
+	}
+
+}
