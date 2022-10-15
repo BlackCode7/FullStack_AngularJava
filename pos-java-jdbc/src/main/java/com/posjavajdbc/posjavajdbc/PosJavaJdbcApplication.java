649779ca7a943d64fa40088b603adeb62bd9bc87
BlackCode7 <seven.metais@outlook.com>
2022-10-15 17:15:35 -0300

    Início do projeto de estudos da lib JDBC para banco postgres JAVA

---
+++ pos-java-jdbc/src/main/java/com/posjavajdbc/posjavajdbc/PosJavaJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.posjavajdbc.posjavajdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosJavaJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosJavaJdbcApplication.class, args);
+	}
+
+}
---
+++ pos-java-jdbc/src/test/java/com/posjavajdbc/posjavajdbc/PosJavaJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posjavajdbc.posjavajdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosJavaJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
